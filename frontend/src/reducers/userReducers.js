a1d380c75ec08823f34ef291b302403f0345b86a
ivan-angelov <62991796+ivan-angelov@users.noreply.github.com>
2021-02-04 08:37:01 +0200

    register user reducer and action

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,13 +1,26 @@
-import { LOGIN_REQUEST, LOGIN_SUCCESS, CLEAR_ERRORS, LOGIN_FAIL } from '../constans/userConstants';
+import { 
+    LOGIN_REQUEST, 
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+
+    REGISTER_USER_REQUEST,
+    REGISTER_USER_SUCCESS,
+    REGISTER_USER_FAIL,
+
+    CLEAR_ERRORS,
+} from '../constans/userConstants';
 
 export const authReducer = (state = { user: {} }, action) => {
     switch (action.type) {
+        
         case LOGIN_REQUEST:
+        case REGISTER_USER_REQUEST:
             return {
                 loading: true,
                 isAuthenticated: false, 
             }
         case LOGIN_SUCCESS:
+        case REGISTER_USER_SUCCESS:
             return {
                 ...state,
                 loading: false,
@@ -15,6 +28,7 @@ export const authReducer = (state = { user: {} }, action) => {
                 user: action.payload
             }
         case LOGIN_FAIL:
+        case REGISTER_USER_FAIL:
             return {
                 ...state,
                 loading: false,
