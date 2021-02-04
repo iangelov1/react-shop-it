2e44cdd6f18d9c62ce45f755ce4181db85105e14
ivan-angelov <62991796+ivan-angelov@users.noreply.github.com>
2021-02-04 12:04:20 +0200

    show user profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import Register from './components/user/Register';
 
 import { loadUser } from './actions/userActions';
 import store from './store';
+import Profile from './components/user/Profile';
 
 const App = () => {
     useEffect(() => {
@@ -31,6 +32,7 @@ const App = () => {
 
                     <Route path="/login" component={Login} />
                     <Route path="/register" component={Register} />
+                    <Route path="/me" component={Profile} />
                 </div>
                 <Footer />
             </div>
