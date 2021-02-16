fdb5aa892f0250e760d527a645dae0083704f7ae
ivan-angelov <62991796+ivan-angelov@users.noreply.github.com>
2021-02-16 16:33:25 +0200

    admin sidebar component / dashboard component / display all products admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,33 +8,39 @@ import Home from './components/Home';
 import Footer from './components/layout/Footer';
 import Header from './components/layout/Header';
 import ProductDetails from './components/products/ProductDetails';
+
+// Cart Imports
+import Cart from './components/cart/Cart';
+import Shipping from './components/cart/Shipping';
+import ConfirmOrder from './components/cart/ConfirmOrder';
+import Payment from './components/cart/Payment';
+import OrderSuccess from './components/cart/OrderSuccess';
+
+// Order Import
+import ListOrders from './components/order/ListOrders';
+import OrderDetails from './components/order/OrderDetails';
+
+//  Auth or User Imports
 import Login from './components/user/Login';
 import Register from './components/user/Register';
-
-import { loadUser } from './actions/userActions';
-
-import store from './store';
 import Profile from './components/user/Profile';
-import ProtectedRoute from './components/route/ProtectedRoute'
 import UpdateProfile from './components/user/UpdateProfile';
 import UpdatePassword from './components/user/UpdatePassword';
 import ForgotPassword from './components/user/ForgotPassword';
 import NewPassword from './components/user/NewPassword';
-import Cart from './components/cart/Cart';
-import Shipping from './components/cart/Shipping';
-import ConfirmOrder from './components/cart/ConfirmOrder';
 
-import Payment from './components/cart/Payment';
-import OrderSuccess from './components/cart/OrderSuccess';
+// Admin Imports
+import Dashboard from './components/admin/Dashboard';
+import ProductsList from './components/admin/ProductsList';
 
-import ListOrders from './components/order/ListOrders';
-
+import ProtectedRoute from './components/route/ProtectedRoute';
+import { loadUser } from './actions/userActions';
+import store from './store';
 import axios from 'axios';
 
 // Payment
 import { Elements } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
-import OrderDetails from './components/order/OrderDetails';
 
 const App = () => {
 
@@ -86,6 +92,10 @@ const App = () => {
                     <ProtectedRoute path="/orders/me" component={ListOrders} exact />
                     <ProtectedRoute path="/order/:id" component={OrderDetails} exact />
                 </div>
+
+                <ProtectedRoute path="/dashboard" isAdmin={true} component={Dashboard} exact />
+                <ProtectedRoute path="/admin/products" isAdmin={true} component={ProductsList} exact />
+
                 <Footer />
             </div>
         </Router>
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,7 +1,7 @@
 import { 
     ALL_PRODUCTS_REQUEST, 
     ALL_PRODUCTS_SUCCESS, 
-    ALL_PRODUCTS_FAIL, 
+    ALL_PRODUCTS_FAIL,
 
     PRODUCT_DETAILS_REQUEST,
     PRODUCT_DETAILS_SUCCESS,
@@ -12,12 +12,17 @@ import {
     NEW_REVIEW_RESET,
     NEW_REVIEW_FAIL,
 
+    ADMIN_PRODUCTS_REQUEST,
+    ADMIN_PRODUCTS_SUCCESS ,
+    ADMIN_PRODUCTS_FAIL,
+
     CLEAR_ERRORS 
 } from '../constans/productConstants';
 
 export const productsReducer = (state = { products:[] }, action ) => {
     switch(action.type) {
         case ALL_PRODUCTS_REQUEST:
+        case ADMIN_PRODUCTS_REQUEST:
             return {
                 loading: true,
                 products: []
@@ -31,8 +36,16 @@ export const productsReducer = (state = { products:[] }, action ) => {
                 resPerPage: action.payload.resPerPage,
                 filteredProductsCount: action.payload.filteredProductsCount
             }
+        
+        case ADMIN_PRODUCTS_SUCCESS:
+            return {
+                loading: false,
+                products: action.payload
+            }
+        
 
         case ALL_PRODUCTS_FAIL:
+        case ADMIN_PRODUCTS_FAIL:
             return {
                 loading: false,
                 error: action.payload
