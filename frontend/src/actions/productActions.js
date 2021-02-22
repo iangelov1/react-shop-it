51edf12473bfb5aba577a932e3e4ea17d4caac90
ivan-angelov <62991796+ivan-angelov@users.noreply.github.com>
2021-02-22 15:48:02 +0200

    delete product

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -20,6 +20,11 @@ import {
     NEW_PRODUCT_SUCCESS,
     NEW_PRODUCT_FAIL,
 
+    DELETE_PRODUCT_REQUEST,
+    DELETE_PRODUCT_SUCCESS,
+    // DELETE_PRODUCT_RESET,
+    DELETE_PRODUCT_FAIL,
+
     CLEAR_ERRORS 
 } from '../constans/productConstants';
 
@@ -137,6 +142,27 @@ export const getAdminProducts = () => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: ADMIN_PRODUCTS_FAIL,
+            payload: error.response.data.message
+        })
+    }
+};
+
+// Delete product (Admin)
+export const deleteProduct = (id) => async (dispatch) => {
+    try {
+
+        dispatch({ type: DELETE_PRODUCT_REQUEST })
+
+        const { data } = await axios.delete(`/api/v1/admin/product/${id}`)
+
+        dispatch({
+            type: DELETE_PRODUCT_SUCCESS,
+            payload: data.success
+        })
+
+    } catch (error) {
+        dispatch({
+            type: DELETE_PRODUCT_FAIL,
             payload: error.response.data.message
         })
     }
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -21,6 +21,11 @@ import {
     NEW_PRODUCT_RESET,
     NEW_PRODUCT_FAIL,
 
+    DELETE_PRODUCT_REQUEST,
+    DELETE_PRODUCT_SUCCESS,
+    DELETE_PRODUCT_RESET,
+    DELETE_PRODUCT_FAIL,
+
     CLEAR_ERRORS 
 } from '../constans/productConstants';
 
@@ -162,6 +167,45 @@ export const newProductReducer = (state = { product: {} }, action) => {
             return {
                 ...state,
                 success: false
+            }
+
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null
+            }
+
+        default:
+            return state
+    }
+};
+
+export const productReducer = (state = {}, action) => {
+    switch (action.type) {
+
+        case DELETE_PRODUCT_REQUEST:
+            return {
+                ...state,
+                loading: true
+            }
+
+        case DELETE_PRODUCT_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                isDeleted: action.payload
+            }
+
+        case DELETE_PRODUCT_FAIL:
+            return {
+                ...state,
+                error: action.payload
+            }
+
+        case DELETE_PRODUCT_RESET:
+            return {
+                ...state,
+                isDeleted: false
             }
 
         case CLEAR_ERRORS:
